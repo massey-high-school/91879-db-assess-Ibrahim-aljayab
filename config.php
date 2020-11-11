1fae011d7fa2c466deadb0f8b9bd759b047649e6
Ibrahim-aljayab <60583427+Ibrahim-aljayab@users.noreply.github.com>
2020-11-12 12:04:59 +1300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', 'On'); // change to 'Off' when live
+
+define ("DB_NAME","aljayabi66849");
+define("DB_USERNAME","_aljayabi66849");
+define("DB_PASSWORD","4fbe7bb1eecb111bf3538450dddcb3d5");
+define("DB_HOST","localhost");
+
+?>
